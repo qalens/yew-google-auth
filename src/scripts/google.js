d5bf4344b099e30ef92959b7b09417703964c0f9
ATM <naik.atmaram@gmail.com>
2021-02-10 18:42:50 +0530

    ATM | removed log

--- src/scripts/google.js
+++ src/scripts/google.js
@@ -5,9 +5,7 @@ export function render_with_callback(clientId,callback) {
         // Retrieve the singleton for the GoogleAuth library and set up the client.
         auth2 = gapi.auth2.init({
             client_id: clientId,
-            cookiepolicy: 'single_host_origin',
-            // Request scopes in addition to 'profile' and 'email'
-            //scope: 'additional_scope'
+            cookiepolicy: 'single_host_origin'
         });
         attachSignin(document.getElementById('login-with-google'),callback);
     });
@@ -17,8 +15,6 @@ function attachSignin(element,callback) {
     console.log(element.id);
     auth2.attachClickHandler(element, {},
         function(googleUser) {
-            alert("Logged In for user "+googleUser.getBasicProfile().getName())
-            console.log(googleUser.getAuthResponse(true));
             callback(googleUser.getAuthResponse(true))
         }, function(error) {
             alert(JSON.stringify(error, undefined, 2));
