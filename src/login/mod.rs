89d4014fdbc70a83ee801aaafb284abe5e8fe341
ATM <naik.atmaram@gmail.com>
2021-02-10 18:37:49 +0530

    ATM | added style

--- src/login/mod.rs
+++ src/login/mod.rs
@@ -15,11 +15,11 @@ pub struct Button{
     pub props:ButtonProps
 }
 fn default_caption()->String{
-    format!("Login With Google")
+    format!("SignIn With Google")
 }
 #[derive(Clone,Properties)]
 pub struct ButtonProps{
-    #[props(default = "default_caption")]
+    #[prop_or_else(default_caption)]
     pub caption:String,
     pub client_id:String,
     pub on_login:Callback<AuthResponse>
@@ -43,14 +43,14 @@ impl Component for Button {
     fn view(&self) -> Html {
         html! {
                 <div>
-                    <div id="login-with-google">
-                      <span>{"Sign In With Google"}</span>
+                    <div id="login-with-google" style="border:1px solid black;cursor:pointer;">
+                      {&self.props.caption}
                     </div>
                 </div>
             }
     }
 
-    fn rendered(&mut self, first_render: bool) {
+    fn rendered(&mut self, _first_render: bool) {
         let callback = self.props.on_login.clone();
         crate::scripts::render_with_callback(self.props.client_id.clone(),Closure::once_into_js( move |resp:JsValue|{
             callback.emit(resp.into_serde().unwrap());
